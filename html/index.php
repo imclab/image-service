5e398e5d63253ccde4e6fb1bf8bb25e4533a4428
Kornel Lesiński <kornel.lesinski@ft.com>
2014-02-07 16:01:33 +0000

    Customised PHP project template

--- html/index.php
+++ html/index.php
@@ -6,24 +6,6 @@
  * @copyright The Financial Times Limited [All Rights Reserved]
  */
 
-require_once __DIR__."/../app/global.php";
+namespace FTLabs;
 
-// Clean up the URI value provided by PHP to determine the controller to be used for this request
-$uri = $_SERVER["REQUEST_URI"];
-$uri = (strpos($uri, "?") !== false) ? substr($uri, 0, strpos($uri, "?")) : $uri;
-$uri = ($uri) ? "/".trim($uri, "/") : "/";
 
-// Set default uri
-if (empty($uri) or ($uri == "/")) {
-	$uri = "/hello";
-}
-
-switch ($uri) {
-	case "/hello":
-	case "/goodbye":
-	case "/compliment":
-		require PROJROOT."/app/controllers/".substr($uri, 1).".php";
-		break;
-	default:
-		require PROJROOT."/app/controllers/404.php";
-}
